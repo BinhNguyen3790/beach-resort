867671486b734490acefe1e2205807b56d339c01
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-03-09 22:52:36 +0700

    creater pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div class="alert alert-info" role="alert">
-      <h1 className="red">Beach Resort</h1>
-      </div>
+      <h1>Hello World!</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rooms = () => {
+  return (
+    <div>
+      <h3>rooms pages</h3>
+    </div>
+  )
+}
+
+export default Rooms
