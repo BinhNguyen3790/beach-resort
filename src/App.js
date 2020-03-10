d49af15198888e0e40c7fbb74d63684359cbeebd
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-03-10 22:46:13 +0700

    creater router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import Error from './pages/Error';
+import SingleRoom from './pages/SingleRoom';
+import Rooms from './pages/Rooms';
+import { Router, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <Home />
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/rooms' component={Rooms}/>
+      <Route exact path='/single-room' component={SingleRoom}/>
     </div>
   );
 }
